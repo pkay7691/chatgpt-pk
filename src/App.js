0b2d89e06e008fe73356123f9a240fde34cca490
Patrick Kenny <patrickmkenny91@gmail.com>
2023-03-25 12:48:24 -0500

    frontend

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import logo from "./logo.svg";
 import "./App.css";
-import './normal.css'
+import "./normal.css";
 
 function App() {
   return (
     <div className="App">
       <aside className="sidemenu">
         <h1>Aside</h1>
+        <div className="sidemenu-button">
+          <span>+</span>
+          New Chat
+        </div>
       </aside>
-      <section className='chatbox'>
-
+      <section className="chatbox">
+        <div className="chat-log">
+          <div className="chat-message">
+            <div className="chat-message-center">
+              <div className="avatar"></div>
+              <div className="message">Hello World</div>
+            </div>
+          </div>
+          <div className="chat-message chatgpt">
+            <div className="chat-message-center">
+              <div className="avatar chatgpt"></div>
+              <div className="message">I am an AI</div>
+            </div>
+          </div>
+        </div>
+        <div className="chatbox-input-holder">
+          <textarea
+            className="chat-input"
+            placeholder="Pleas type here"
+            rows="1"
+          ></textarea>
+        </div>
       </section>
-     
     </div>
   );
 }
