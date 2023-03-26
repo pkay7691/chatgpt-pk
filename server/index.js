1363fb54b83d6512df54fab283e91a7b795942bc
Patrick Kenny <patrickmkenny91@gmail.com>
2023-03-26 13:05:16 -0500

    message styling

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 // 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
 const  { Configuration, OpenAIApi } = require ("openai");
+const express = require('express')
+
 const configuration = new Configuration({
     organization: "org-lkAuakCMwFaLoHEQtdWeU0GZ",
-    apiKey: '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',
+    apiKey: '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',
 });
 const openai = new OpenAIApi(configuration);
 
@@ -18,4 +20,30 @@ async function callApi() {
   console.log(response.data.choices[0].text)
 }
 
-callApi()
+
+// create a simple express api that calls the function above
+
+
+const app = express()
+const port = 3080
+
+app.post('/',  async (req, res) => {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      max_tokens: 7,
+      temperature: 0,
+    });
+    console.log(response.data.choices[0].text)
+    res.json({
+      data: response.data
+    })
+  })
+
+
+app.listen(port, () => {
+  console.log(`Chatgpt-PK listening at http://localhost:${port}`)
+})
+
+
+
